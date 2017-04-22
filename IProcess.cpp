667bb76bfc4f9b7dc63bc99687c2e28a2b895667
leo Le Diouron <leo.le-diouron@epitech.eu>
2017-04-22 15:04:37 +0200

    rm I.cpp

--- IProcess.cpp
+++
@@ -1,6 +0,0 @@
-#include "IProcess.hpp"
-
-Plazza::Controller::IProcess::IProcess() {
-	// TODO - implement IProcess::IProcess
-	throw "Not yet implemented";
-}
--- IThreadsPool.cpp
+++
@@ -1,6 +0,0 @@
-#include "IThreadsPool.hpp"
-
-Plazza::Controller::IThreadsPool::IThreadsPool() {
-	// TODO - implement IThreadsPool::IThreadsPool
-	throw "Not yet implemented";
-}
