5e842de8caf6c50752add2a3c13dd18785f039ac
leo Le Diouron <leo.le-diouron@epitech.eu>
2017-04-30 14:45:06 +0200

    ok

--- Sources/Process/Socket.cpp
+++ Sources/Process/Socket.cpp
@@ -26,45 +26,38 @@ int 										Plazza::Controller::Socket::socketChild()
     struct hostent 			*server;
 
     if ((this->_port > 65535) || (this->_port < 2000)) {
-        std::cerr << "Please enter port number between 2000 - 65535" << std::endl;
-        return 0;
+      std::cerr << "Please enter port number between 2000 - 65535" << std::endl;
+      return 0;
     }
-
     listenFd = socket(AF_INET, SOCK_STREAM, 0);
-
     if (listenFd < 0) {
-        std::cerr << "Cannot open socket" << std::endl;
-        return 0;
+      std::cerr << "Cannot open socket" << std::endl;
+      return 0;
     }
-
     server = gethostbyname("localhost");
-
     if (server == NULL) {
-        std::cerr << "Host does not exist" << std::endl;
-        return 0;
+      std::cerr << "Host does not exist" << std::endl;
+      return 0;
     }
-
     bzero((char *)&svrAdd, sizeof(svrAdd));
     svrAdd.sin_family = AF_INET;
-
     bcopy((char *) server->h_addr, (char *)&svrAdd.sin_addr.s_addr, server->h_length);
-
     svrAdd.sin_port = htons(this->_port);
-
     int checker = connect(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd));
-
     if (checker < 0) {
-        std::cerr << "Cannot connect port " << this->_port << std::endl;
-        return 0;
+      std::cerr << "Cannot connect port " << this->_port << std::endl;
+      return 0;
     }
     return (listenFd);
 }
 
 int Plazza::Controller::Socket::socketParent()
 {
-    int 							 listenFd, connFd;
+    int 							 listenFd;
+    int								 connFd;
     socklen_t 				 len;
-    struct sockaddr_in svrAdd, clntAdd;
+    struct sockaddr_in svrAdd;
+    struct sockaddr_in clntAdd;
 
     if ((this->_port > 65535) || (this->_port < 2000)) {
         std::cerr << "Please enter a port number between 2000 - 65535" << std::endl;
@@ -77,18 +70,14 @@ int Plazza::Controller::Socket::socketParent()
     }
     setsockopt(listenFd, SOL_SOCKET, SO_REUSEADDR, (char *)&len, (int)sizeof(len));
     bzero((char *)&svrAdd, sizeof(svrAdd));
-
     svrAdd.sin_family = AF_INET;
     svrAdd.sin_addr.s_addr = INADDR_ANY;
     svrAdd.sin_port = htons(this->_port);
-
     if (bind(listenFd, (struct sockaddr *)&svrAdd, sizeof(svrAdd)) < 0) {
-        std::cerr << "Cannot bind" << std::endl;
-        return 0;
+      std::cerr << "Cannot bind" << std::endl;
+      return 0;
     }
-
     listen(listenFd, 5);
-
     len = sizeof(clntAdd);
     connFd = accept(listenFd, (struct sockaddr *)&clntAdd, &len);
     return (connFd);
